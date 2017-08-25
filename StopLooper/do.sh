edd02d40c9420e6738a925e3da049d68b7a7cb45
wsicheng <sicheng@physics.ucsb.edu>
2017-08-24 22:16:50 -0700

    Continue to put in stop region definitions

--- StopLooper/do.sh
+++ StopLooper/do.sh
@@ -5,13 +5,13 @@ make -j 12 || return $?
 
 INDIR=/nfs-7/userdata/haweber/stopbabiesInteractive/2017_5p8fbinv
 
-OUTDIR=output/data2017
+OUTDIR=output/temp
 LOGDIR=logs
 mkdir -p ${OUTDIR}
 mkdir -p ${LOGDIR}
 
-declare -a Samples=(data_singlemu)
-# declare -a Samples=(data_singlemu data_singleel data_met data_jetht)
+# declare -a Samples=(data_singlemu)
+declare -a Samples=(data_singlemu data_singleel data_met data_jetht)
 
 for SAMPLE in ${Samples[@]}; do
     echo ./runStopLooper ${INDIR} ${SAMPLE} ${OUTDIR}
@@ -25,7 +25,6 @@ done
 # 2016 data for comparison
 INDIR=/nfs-7/userdata/stopRun2/analysis2016_SUS-16-051_35p9fbinv/v24/skim
 OUTDIR=output/data2016
-
 mkdir -p ${OUTDIR}
 mkdir -p ${LOGDIR}
 
@@ -36,7 +35,6 @@ for SAMPLE in ${Samples[@]}; do
     echo ./runStopLooper ${INDIR} ${SAMPLE} ${OUTDIR}
     # ./runStopLooper ${INDIR} ${SAMPLE} ${OUTDIR}
     eval "nohup ./runStopLooper ${INDIR} ${SAMPLE} ${OUTDIR} >& ${LOGDIR}/log_${SAMPLE}.txt &"
-
     # echo nice -n 10 ./runStopLooper ${INDIR} ${SAMPLE} ${OUTDIR}
     # eval "nohup nice -n 10 ./runStopLooper ${INDIR} ${SAMPLE} ${OUTDIR} >& ${LOGDIR}/log_${SAMPLE}_2016.txt &"
 done
@@ -48,15 +46,13 @@ OUTDIR=output/mc2016
 mkdir -p ${OUTDIR}
 mkdir -p ${LOGDIR}
 
-declare -a Samples=(ttbar)
-# declare -a Samples=(DYJetsToLL TTGJets TTWJets TTZ W1Jets W2Jets W3Jets W4Jets W4Jets WJets WW WZ ZZ tZq ttbar ttWJets ttZJets t_sch t_tW t_tbarW t_tch tbar_tch)
-
+# declare -a Samples=(ttbar)
+declare -a Samples=(DYJetsToLL TTGJets TTWJets TTZ W1Jets W2Jets W3Jets W4Jets W4Jets WJetsToLNu WW WZ ZZ tZq ttbar ttWJets ttZJets t_sch t_tW t_tbarW t_tch tbar_tch)
 
 for SAMPLE in ${Samples[@]}; do
     echo ./runStopLooper ${INDIR} ${SAMPLE} ${OUTDIR}
     # ./runStopLooper ${INDIR} ${SAMPLE} ${OUTDIR}
     eval "nohup ./runStopLooper ${INDIR} ${SAMPLE} ${OUTDIR} >& ${LOGDIR}/log_${SAMPLE}.txt &"
-
     # echo nice -n 10 ./runStopLooper ${INDIR} ${SAMPLE} ${OUTDIR}
     # eval "nohup nice -n 10 ./runStopLooper ${INDIR} ${SAMPLE} ${OUTDIR} >& ${LOGDIR}/log_${SAMPLE}_2016.txt &"
 done
