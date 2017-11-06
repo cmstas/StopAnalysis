e4acabb68cf3632d63f0dd99af557ac0dc82121b
wsicheng <sicheng@physics.ucsb.edu>
2017-11-05 21:01:58 -0800

    Allow running directly from dataset input in metis

--- StopBabyMaker/batch_metis/submit.py
+++ StopBabyMaker/batch_metis/submit.py
@@ -1,17 +1,11 @@
 from __future__ import print_function
 
 import time
-import os
-
-import itertools
-import json
-import traceback
 
 from metis.Sample import SNTSample
 from metis.Sample import DirectorySample
 from metis.CondorTask import CondorTask
 from metis.StatsParser import StatsParser
-from metis.Utils import send_email
 from metis.Utils import do_cmd
 
 
@@ -19,46 +13,75 @@ if __name__ == "__main__":
 
     dataset_names = [
         # "/TT_TuneCUETP8M2T4_13TeV-powheg-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10_ext1-v1/MINIAODSIM",
-        "/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v3/MINIAODSIM",
+        # "/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v3/MINIAODSIM",
+        "/SMS-T8bbstausnu_XCha0p5_XStau0p25_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-PUSummer16Fast_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM",
+        "/SMS-T8bbstausnu_XCha0p5_XStau0p5_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-PUSummer16Fast_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM",
+        "/SMS-T8bbstausnu_XCha0p5_XStau0p75_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-PUSummer16Fast_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM",
     ]
 
     # Make a directory sample, giving it the location and a dataset name for bookkeeping purposes
     # The globber must be customized (by default, it is *.root) in order to pick up the text files
 
     # Make a CondorTask (3 in total, one for each input)
-    task = CondorTask(
-            sample = SNTSample(dataset=dataset_names[0]),
-            files_per_output = 1,
-            tag = "v25_2",
-            output_name = "TTJets_V07_test.root",
-            executable = "condor_executable.sh",
-            cmssw_version = "CMSSW_9_4_0_pre3",
-            scram_arch = "slc6_amd64_gcc700",
-            arguments = "testarg1",
-            tarfile = "input.tar.gz",
-            condor_submit_params = {"sites": "UAF,T2_US_UCSD,UCSB"},
-            no_load_from_backup = True, # for the purpose of the example, don't use a backup
-    )
-    # do_cmd("rm -rf {0}".format(task.get_outputdir()))
+    maker_tasks = []
+    merge_tasks = []
 
-    # Process and sleep until complete
-    is_complete = False
-    for t in [5.0, 5.0, 10.0, 15.0, 20.0]:
-        task.process()
-        print("Sleeping for {0} seconds".format(int(t)))
-        time.sleep(t)
-        is_complete = task.complete()
-        if is_complete: break
+    for dsname in dataset_names:
+        cmsswver = "CMSSW_9_4_0_pre2"
+        tarfile = "input.tar.gz"
+        tag = "v25_3"
+        maker_task = CondorTask(
+                sample = SNTSample(dataset=dsname),
+                files_per_output = 1,
+                tag = tag,
+                output_name = "stopbaby.root",
+                executable = "condor_executable.sh",
+                cmssw_version = cmsswver,
+                scram_arch = "slc6_amd64_gcc700",
+                arguments = "1", # isFastsim
+                tarfile = tarfile,
+                condor_submit_params = {"sites": "UAF,T2_US_UCSD,UCSB"},
+        )
+        merge_task = CondorTask(
+                sample = DirectorySample( 
+                    dataset=dsname.replace("MINIAODSIM","MERGE"),
+                    location=maker_task.get_outputdir(),
+                ),
+                executable = "metis/executables/condor_hadd.sh",
+                open_dataset = False,
+                files_per_output = 50,
+                output_name = "merged_stopbaby.root",
+                condor_submit_params = {"sites":"UAF"},
+                output_is_tree = True,
+                check_expectedevents = True,
+                tag = tag,
+                cmssw_version = cmsswver,
+        )
+        maker_tasks.append(maker_task)
+        merge_tasks.append(merge_task)
+        
+
+    for i in range(100):
+        total_summary = {}
+            
+        for maker_task, merge_task in zip(maker_tasks,merge_tasks):
+            maker_task.process()
+
+            if maker_task.complete():
+                merge_task.reset_io_mapping()
+                merge_task.update_mapping()
+                merge_task.process()
+
+            total_summary[maker_task.get_sample().get_datasetname()] = maker_task.get_task_summary()
+            total_summary[merge_task.get_sample().get_datasetname()] = merge_task.get_task_summary()
+
+        # parse the total summary and write out the dashboard
+        StatsParser(data=total_summary, webdir="~/public_html/dump/metis/").do()
+
+        # 1 hr power nap
+        time.sleep(10.*60)
 
     # If it's complete, make a dummy sample out of the output directory
     # in order to pick up the files. Then cat out the contents and sum
     # them up. This should be 3*2*10 = 100
-    if is_complete:
-        print("Job completed! Checking outputs...")
-        outsamp = DirectorySample(location=task.get_outputdir(), dataset="/Blah/blah/BLAH", globber="*.txt")
-        tot = 0
-        for f in outsamp.get_files():
-            mf = MutableFile(f.get_name())
-            tot += int(mf.cat())
-        print("It looks like we found 3*2*10 = {0}".format(tot))
 
