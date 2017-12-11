8e468ac7bf1d40f3499deb0ca5f4c39a211d253e
wsicheng <sicheng@physics.ucsb.edu>
2017-12-10 17:17:40 -0800

    Plot full 2017 data

--- AnalyzeScripts/doAll.C
+++ AnalyzeScripts/doAll.C
@@ -1,7 +1,7 @@
 {
 
   cout << "Loading dataMCplotMaker...\n";
-  // gROOT->ProcessLine(".L ~/tas/Software/dataMCplotMaker/dataMCplotMaker.cc+");
+  // gROOT->ProcessLine(".L ~/tas/Software/dataMCplotMaker/dataMCplotMaker.cc++");
   gSystem->Load("~/tas/Software/dataMCplotMaker/dataMCplotMaker_cc.so");
   gROOT->ProcessLine(".L compare_data_17vs16.C+");
   gROOT->ProcessLine("compare_data_17vs16()");
