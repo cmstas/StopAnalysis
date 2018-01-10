bd5d05f79fae9ee08930292b36fd8280a00477c4
wsicheng <sicheng@physics.ucsb.edu>
2018-01-10 14:47:01 -0800

    Move min value for pt to match the btageff hist and add warning

--- StopBabyMaker/doTest.sh
+++ StopBabyMaker/doTest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-make -j 12 || return $?
+[[ -f Makefile ]] && { make -j 12 || return $?; }
 
 # runBabyMaker takes six arguments: ./runBabyMaker sample_name nevents file_number outpath samplelist isFastSim
 #  Need to provide at least sample_name; nevents=-1 (-1=all events), file_number=-1 (-1=merged_ntuple_*.root), output=/nfs-7/userdata/stopRun2/, samplelist=sample_2017.dat, isFastSim=false by default
@@ -20,4 +20,4 @@ make -j 12 || return $?
 # ./runBabyMaker ttbar_madgraph_pythia8_25ns 10000 1 
 # ./runBabyMaker stopbaby -1 55 ./ /hadoop/cms/store/user/namin/ProjectMetis/DoubleMuon_Run2017D-PromptReco-v1_MINIAOD_CMS4_V00-00-06/merged_ntuple_55.root 0 
 
-./runBabyMaker stopbaby -1 103 ./ /hadoop/cms/store/user/sicheng/ProjectMetis/resub_data_2017Cv3_singlemu_v25_7_stopbaby_1_test3//stopbaby_103_1.root 0 
+./runBabyMaker stopbaby 3000 4 ./ /hadoop/cms/store/user/sicheng/ProjectMetis/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_RunIISummer17MiniAOD-92X_upgrade2017_realistic_v10-v3_MINIAODSIM_CMS4_V00-00-07_test/merged_ntuple_12.root 0 
