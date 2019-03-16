b547c2d15ff2b609adc4f12633df5cc6ca1b725e
wsicheng <sicheng@physics.ucsb.edu>
2019-03-16 08:27:42 -0700

    Update the sample list for StopLooper

--- StopLooper/do.sh
+++ StopLooper/do.sh
@@ -16,20 +16,20 @@ skimtype=skimmed
 
 OUTDIR18=$OUTDIR
 
-OUTDIR18=output/samp18_v37_s7
-OUTDIR17=output/samp17_v39_s1
-OUTDIR16=output/samp16_v39_s1
-OUTDIRRUN2=output/combRun2_v39_s1
+OUTDIR18=output/samp18_v39_s4
+OUTDIR17=output/samp17_v39_s4
+OUTDIR16=output/samp16_v39_s4
+OUTDIRRUN2=output/combRun2_v39_s4
 
-run18data=1
+run18dat=1
 run18bkg=1
 run18sig=1
 
-run17data=1
+run17dat=1
 run17bkg=1
 run17sig=1
 
-run16data=1
+run16dat=1
 run16bkg=1
 run16sig=1
 
@@ -54,27 +54,33 @@ OUTDIR=$OUTDIR18
 LOGDIR=$OUTDIR/logs
 
 declare -a Samples=(data_2018A data_2018B data_2018C data_2018D)
+# declare -a Samples=(data_2018A_jetht data_2018B_jetht data_2018C_jetht data_2018D_jetht)
 # declare -a Samples=(data_2018 data_2017 data_2016)
 
-[[ $run18data == 1 ]] && runLooperJobs
+[[ $run18dat == 1 ]] && runLooperJobs
 
 ########################
 # 2018 MC
 
 # INDIR=/nfs-7/userdata/sicheng/stopbabies/$skimtype/f17v2_v30_7
-INDIR=/nfs-7/userdata/sicheng/stopbabies/$skimtype/a18v1_v30_7
+INDIR=/nfs-7/userdata/sicheng/stopbabies/$skimtype/a18v1_v30_10
 # OUTDIR=output/samp18_v30
 # LOGDIR=$OUTDIR/logs
 
 # # declare -a Samples=(TTJets_amcnlo_0 TTJets_amcnlo_1 TTJets_amcnlo_2)
 declare -a Samples=()
-Samples+=( TTJets_2lep_a18v1_0 TTJets_2lep_a18v1_1 TTJets_2lep_a18v1_2 )
+Samples+=( TTJets_2lep_a18v1_0 TTJets_2lep_a18v1_1 TTJets_2lep_a18v1_2 TTJets_2lep_a18v1_3 TTJets_2lep_a18v1_4 )
 Samples+=( TTJets_1lep_top TTJets_1lep_tbar )
-Samples+=( ST_tW_top ST_tW_tbar ST_schan ST_tchan )   # singleT (missing some)
+Samples+=( ST_tW_top ST_tW_tbar ST_schan ST_tchan )   # singleT
 Samples+=( W1Jets W2Jets W3Jets W4Jets DYJets )       # Vjets : Wjets + DY
 Samples+=( TTZToLLNuNu TTWJetsToLNu )      # rare  : ttV
-# Samples+=( WZTo1L3Nu WZTo3LNu WZTo2L2Q WWToLNuQQ WWTo2L2Nu )   # diboson
-Samples+=( WZTo3LNu ZZTo2L2Nu )   # diboson
+Samples+=( WZTo1L3Nu WZTo3LNu WZTo2L2Q WWToLNuQQ WWTo2L2Nu ZZ )   # diboson
+
+# Samples+=( TTJets_1lep_top_a18b1_0 TTJets_1lep_top_a18b1_1 TTJets_1lep_top_a18b1_2 )
+# Samples+=( TTJets_1lep_tbar_a18b1_0 TTJets_1lep_tbar_a18b1_1 TTJets_1lep_tbar_a18b1_2 )
+# Samples+=( W1Jets W2Jets W3Jets W4JetsToLNu DYJets )
+# Samples+=( W4Jets_NuPt200_a18v1_0 W4Jets_NuPt200_a18v1_1 W4Jets_NuPt200_a18v1_2 W4Jets_NuPt200_a18v1_3 )
+# Samples+=( W4Jets_NuPt200_a18v1_4 W4Jets_NuPt200_a18v1_5 W4Jets_NuPt200_a18v1_6 )
 
 [[ $run18bkg == 1 ]] && runLooperJobs
 
@@ -104,8 +110,8 @@ Samples+=( SMS_T2tt_mStop150to250_f17v2_ext1_0 SMS_T2tt_mStop150to250_f17v2_ext1
 
 # INDIR=/nfs-7/userdata/haweber/stopbabiesInteractive/2017_5p8fbinv
 # INDIR=/nfs-7/userdata/haweber/tupler_babies/merged/Stop_1l/v2017/output/
-# INDIR=/nfs-7/userdata/sicheng/stopbabies/$skimtype/data_v30_5s
-INDIR=/nfs-7/userdata/sicheng/stopbabies/$skimtype/data_v30_7
+# INDIR=/nfs-7/userdata/sicheng/stopbabies/$skimtype/data_v30_7
+INDIR=/nfs-7/userdata/sicheng/stopbabies/$skimtype/data_v30_5s
 OUTDIR=$OUTDIR17
 # OUTDIR=output/data17_v30_leptrig
 LOGDIR=$OUTDIR/logs
@@ -113,7 +119,7 @@ LOGDIR=$OUTDIR/logs
 # declare -a Samples=(data_2017F)
 declare -a Samples=(data_2017B data_2017C data_2017D data_2017E data_2017F_09May)
 
-[[ $run17data == 1 ]] && runLooperJobs
+[[ $run17dat == 1 ]] && runLooperJobs
 
 ########################
 # 2017 MC
@@ -127,13 +133,18 @@ INDIR=/nfs-7/userdata/sicheng/stopbabies/$skimtype/f17v2_v30_9
 LOGDIR=${OUTDIR}/logs
 
 declare -a Samples=()
-Samples+=( TTJets_2lep_f17v2_0 TTJets_2lep_f17v2_1 TTJets_2lep_f17v2_2 TTJets_2lep_f17v2_3 )
+Samples+=( TTJets_2lep_f17v2_0 TTJets_2lep_f17v2_1 TTJets_2lep_f17v2_2 TTJets_2lep_f17v2_3 TTJets_2lep_f17v2_4 )
 Samples+=( TTJets_1lep_top TTJets_1lep_tbar )
-# Samples+=( TTJets_1lep_top_f17v2_0 TTJets_1lep_top_f17v2_1 TTJets_1lep_tbar_f17v2_0 TTJets_1lep_tbar_f17v2_1 )
-Samples+=( ST_tW_top ST_tW_tbar ST_schan )      # singleT (missing some)
+Samples+=( ST_tW_top ST_tW_tbar ST_schan ST_tchan )   # singleT
 Samples+=( W1Jets W2Jets W3Jets W4Jets DYJets )       # Vjets : Wjets + DY
-Samples+=( TTZToLLNuNu TTWJetsToLNu TTWJetsToQQ )      # rare  : ttV
-Samples+=( WZTo1L3Nu WZTo3LNu WZTo2L2Q WWToLNuQQ WWTo2L2Nu )   # diboson
+Samples+=( TTZToLLNuNu TTWJetsToLNu TTWJetsToQQ TTZToQQ TTZToLL_M1to10 ) # rare  : ttV
+Samples+=( WZTo1L3Nu WZTo3LNu WZTo2L2Q WZTo1L1Nu2Q WWToLNuQQ WWTo2L2Nu TTWZ ZZ )   # diboson
+
+# Samples+=( TTJets_1lep_top_f17v2_0 TTJets_1lep_top_f17v2_1 TTJets_1lep_top_f17v2_2 TTJets_1lep_top_f17v2_3 )
+# Samples+=( TTJets_1lep_tbar_f17v2_0 TTJets_1lep_tbar_f17v2_1 TTJets_1lep_tbar_f17v2_2 )
+# Samples+=( W1Jets W2Jets W3Jets W4JetsToLNu DYJets )       # Vjets : Wjets + DY
+# Samples+=( W4Jets_NuPt200_f17v2_0 W4Jets_NuPt200_f17v2_1 W4Jets_NuPt200_f17v2_2 )
+# Samples+=( W4Jets_NuPt200_f17v2_3 W4Jets_NuPt200_f17v2_4 W4Jets_NuPt200_f17v2_5 )
 
 [[ $run17bkg == 1 ]] && runLooperJobs
 
@@ -172,7 +183,7 @@ LOGDIR=${OUTDIR}/logs
 declare -a Samples=(data_2016B data_2016C data_2016D data_2016E data_2016F data_2016G data_2016H)
 # declare -a Samples=(data_2016)
 
-[[ $run16data == 1 ]] && runLooperJobs
+[[ $run16dat == 1 ]] && runLooperJobs
 
 ########################
 # 2016 MC
@@ -195,14 +206,21 @@ INDIR=/nfs-7/userdata/sicheng/stopbabies/$skimtype/s16v3_v30_9
 # LOGDIR=$OUTDIR/logs
 
 declare -a Samples=()
-Samples+=( TTJets_2lep_s16v3_ext0_1 TTJets_2lep_s16v3_ext1_0 TTJets_2lep_s16v3_ext1_1 TTJets_2lep_s16v3_ext1_2 )
+Samples+=( TTJets_2lep_s16v3_ext0_1 TTJets_2lep_s16v3_ext1_0 TTJets_2lep_s16v3_ext1_1 )
+Samples+=( TTJets_2lep_s16v3_ext1_2 TTJets_2lep_s16v3_ext1_3 )
 Samples+=( TTJets_1lep_top TTJets_1lep_tbar )
-Samples+=( ST_tW_top ST_tW_tbar ST_tchan )      # singleT
+Samples+=( ST_tW_top ST_tW_tbar ST_tchan ST_schan )    # singleT
 Samples+=( W1Jets W2Jets W3Jets W4Jets DYJets )       # Vjets : Wjets + DY
-Samples+=( TTZToLLNuNu TTWJetsToLNu WZTo1L3Nu WWToLNuQQ )  # rare  : ttV + diboson
-Samples+=( WWTo2L2Nu WZTo1L1Nu2Q WZTo2L2Q WZTo3LNu_powheg TTWJetsToQQ TTZToQQ)      # rare : smaller contributions
-# Samples+=( TTJets_1lep_tbar_s16v3_ext0_1  TTJets_1lep_tbar_s16v3_ext1_0  TTJets_1lep_tbar_s16v3_ext1_1 )
-# Samples+=( TTJets_1lep_top_s16v3_ext0_1  TTJets_1lep_top_s16v3_ext1_0  TTJets_1lep_top_s16v3_ext1_1 )
+Samples+=( TTZToLLNuNu TTWJetsToLNu TTWJetsToQQ TTZToQQ )  # rare  : ttV
+Samples+=( WZTo1L3Nu WZTo1L1Nu2Q WZTo3LNu_powheg WZTo2L2Q WWTo2L2Nu WWToLNuQQ ZZ )      # rare : smaller contributions
+
+# Samples+=( TTJets_1lep_tbar_s16v3_ext0_1 TTJets_1lep_tbar_s16v3_ext1_0 )
+# Samples+=( TTJets_1lep_tbar_s16v3_ext1_1 TTJets_1lep_tbar_s16v3_ext1_2 )
+# Samples+=( TTJets_1lep_top_s16v3_ext0_1 TTJets_1lep_top_s16v3_ext1_0 )
+# Samples+=( TTJets_1lep_top_s16v3_ext1_1 TTJets_1lep_top_s16v3_ext1_2 )
+# Samples+=( W4Jets_NuPt200_s16v3_0 W4Jets_NuPt200_s16v3_1 W4Jets_NuPt200_s16v3_2 )
+# Samples+=( W4Jets_NuPt200_s16v3_3 W4Jets_NuPt200_s16v3_4 W4Jets_NuPt200_s16v3_5 )
+# Samples+=( W1Jets W2Jets W3Jets W4JetsToLNu DYJets )       # Vjets : Wjets + DY
 
 [[ $run16bkg == 1 ]] && runLooperJobs
 
@@ -226,6 +244,7 @@ Samples+=( SMS_T2tt_mStop350to400_s16v3_3 SMS_T2tt_mStop350to400_s16v3_4)
 Samples+=( SMS_T2tt_mStop250to350_s16v3_0 SMS_T2tt_mStop250to350_s16v3_1 )
 Samples+=( SMS_T2tt_mStop250to350_s16v3_2 SMS_T2tt_mStop250to350_s16v3_3 )
 Samples+=( SMS_T2tt_mStop150to250_s16v3_0 SMS_T2tt_mStop150to250_s16v3_1)
+
 # Samples+=( SMS_T2tt_400to1200_80X_0 SMS_T2tt_400to1200_80X_1 SMS_T2tt_400to1200_80X_2 )
 # Samples+=( SMS_T2tt_400to1200_80X_3 SMS_T2tt_400to1200_80X_4 )
 # Samples+=( SMS_T2bW_80X_0 SMS_T2bW_80X_1 SMS_T2bW_80X_2 SMS_T2bW_80X_3 SMS_T2bW_80X_4 )
@@ -259,40 +278,40 @@ echo -e 'All looper jobs done!\a'
 
 # Local merge for the v29_11+ babies
 pushd ${OUTDIR18}
-[[ $run18bkg  == 1 ]] && hadd -f ttbar_18.root    TTJets*.root > /dev/null
-[[ $run18bkg  == 1 ]] && hadd -f singleT_18.root  ST_*.root  > /dev/null
-[[ $run18bkg  == 1 ]] && hadd -f Vjets_18.root    W?Jets*.root DYJets*.root > /dev/null
-# [[ $run18bkg  == 1 ]] && hadd -f rare_18.root     TTZ*.root TTW*.root WZ*.root WW*.root > /dev/null
-[[ $run18bkg  == 1 ]] && hadd -f rare_18.root     TTZ*.root TTW*.root WZ*.root > /dev/null
-[[ $run18bkg  == 1 ]] && hadd -f allBkg_18.root   ttbar_18.root singleT_18.root Vjets_18.root rare_18.root > /dev/null
-[[ $run18data == 1 ]] && hadd -f allData_18.root  data_2018*.root > /dev/null
-[[ $run18sig  == 1 ]] && hadd -f SMS_T2tt_18.root SMS_T2tt_*to*.root > /dev/null
+[[ $run18bkg == 1 ]] && hadd -f ttbar_18.root    TTJets*.root > /dev/null
+[[ $run18bkg == 1 ]] && hadd -f singleT_18.root  ST_*.root  > /dev/null
+[[ $run18bkg == 1 ]] && hadd -f Vjets_18.root    W?Jets*.root DYJets*.root > /dev/null
+[[ $run18bkg == 1 ]] && hadd -f rare_18.root     TTZ*.root TTW*.root WZ*.root WW*.root ZZ*.root > /dev/null
+# [[ $run18bkg == 1 ]] && hadd -f rare_18.root     TTZ*.root TTW*.root WZ*.root > /dev/null
+[[ $run18bkg == 1 ]] && hadd -f allBkg_18.root   ttbar_18.root singleT_18.root Vjets_18.root rare_18.root > /dev/null
+[[ $run18dat == 1 ]] && hadd -f allData_18.root  data_2018*.root > /dev/null
+[[ $run18sig == 1 ]] && hadd -f SMS_T2tt_18.root SMS_T2tt_*to*.root > /dev/null && rm SMS_T2tt_*to*.root
 popd > /dev/null
 
 # Local merge for the v29_11+ babies
 pushd ${OUTDIR17}
-[[ $run17bkg  == 1 ]] && hadd -f ttbar_17.root    TTJets*.root > /dev/null
-[[ $run17bkg  == 1 ]] && hadd -f singleT_17.root  ST_*.root  > /dev/null
-# [[ $run17bkg  == 1 ]] && hadd -f Vjets_17.root    W?Jets*.root DYJets*.root > /dev/null
-[[ $run17bkg  == 1 ]] && hadd -f Vjets_17.root    W?Jets*.root > /dev/null
-[[ $run17bkg  == 1 ]] && hadd -f rare_17.root     TTZ*.root TTW*.root WZ*.root WW*.root > /dev/null
-[[ $run17bkg  == 1 ]] && hadd -f allBkg_17.root   ttbar_17.root singleT_17.root Vjets_17.root rare_17.root > /dev/null
+[[ $run17bkg == 1 ]] && hadd -f ttbar_17.root    TTJets*.root > /dev/null
+[[ $run17bkg == 1 ]] && hadd -f singleT_17.root  ST_*.root  > /dev/null
+[[ $run17bkg == 1 ]] && hadd -f Vjets_17.root    W?Jets*.root DYJets*.root > /dev/null
+# [[ $run17bkg == 1 ]] && hadd -f Vjets_17.root    W?Jets*.root > /dev/null
+[[ $run17bkg == 1 ]] && hadd -f rare_17.root     TTZ*.root TTW*.root WZ*.root WW*.root ZZ*.root > /dev/null
+[[ $run17bkg == 1 ]] && hadd -f allBkg_17.root   ttbar_17.root singleT_17.root Vjets_17.root rare_17.root > /dev/null
 # [[ $run17data == 1 ]] && hadd -f allData_17.root  data_2017?.root > /dev/null
-[[ $run17data == 1 ]] && hadd -f allData_17.root  data_2017?.root data_2017F_09May.root > /dev/null
-[[ $run17data == 1 ]] && hadd -f data_2017BtoE.root data_2017[B-E].root > /dev/null
-[[ $run17sig  == 1 ]] && hadd -f SMS_T2tt_17.root SMS_T2tt_*to*.root > /dev/null
+[[ $run17dat == 1 ]] && hadd -f allData_17.root  data_2017[B-E].root data_2017F_09May.root > /dev/null
+[[ $run17dat == 1 ]] && hadd -f data_2017BtoE.root data_2017[B-E].root > /dev/null
+[[ $run17sig == 1 ]] && hadd -f SMS_T2tt_17.root SMS_T2tt_*to*.root > /dev/null && rm SMS_T2tt_*to*.root
 popd > /dev/null
 
 pushd ${OUTDIR16}
-[[ $run16bkg  == 1 ]] && hadd -f ttbar_16.root    TTJets*.root > /dev/null
-[[ $run16bkg  == 1 ]] && hadd -f singleT_16.root  ST_*.root  > /dev/null
-[[ $run16bkg  == 1 ]] && hadd -f Vjets_16.root    W?Jets*.root DYJets*.root > /dev/null
-[[ $run16bkg  == 1 ]] && hadd -f rare_16.root     TTZ*.root TTW*.root WZ*.root WW*.root > /dev/null
-[[ $run16bkg  == 1 ]] && hadd -f allBkg_16.root   ttbar_16.root singleT_16.root Vjets_16.root rare_16.root > /dev/null
-[[ $run16data == 1 ]] && hadd -f allData_16.root  data_2016*.root > /dev/null
-[[ $run16sig  == 1 ]] && hadd -f SMS_T2tt_16.root SMS_T2tt_*to*.root > /dev/null
-# [[ $run16sig  == 1 ]] && hadd -f SMS_T2bW_16.root SMS_T2bW_*to*.root > /dev/null
-# [[ $run16sig  == 1 ]] && hadd -f SMS_T2bt_16.root SMS_T2bt_*to*.root > /dev/null
+[[ $run16bkg == 1 ]] && hadd -f ttbar_16.root    TTJets*.root > /dev/null
+[[ $run16bkg == 1 ]] && hadd -f singleT_16.root  ST_*.root  > /dev/null
+[[ $run16bkg == 1 ]] && hadd -f Vjets_16.root    W?Jets*.root DYJets*.root > /dev/null
+[[ $run16bkg == 1 ]] && hadd -f rare_16.root     TTZ*.root TTW*.root WZ*.root WW*.root ZZ*.root > /dev/null
+[[ $run16bkg == 1 ]] && hadd -f allBkg_16.root   ttbar_16.root singleT_16.root Vjets_16.root rare_16.root > /dev/null
+[[ $run16dat == 1 ]] && hadd -f allData_16.root  data_2016*.root > /dev/null
+[[ $run16sig == 1 ]] && hadd -f SMS_T2tt_16.root SMS_T2tt_*to*.root > /dev/null && rm SMS_T2tt_*to*.root
+# [[ $run16sig == 1 ]] && hadd -f SMS_T2bW_16.root SMS_T2bW_*to*.root > /dev/null
+# [[ $run16sig == 1 ]] && hadd -f SMS_T2bt_16.root SMS_T2bt_*to*.root > /dev/null
 popd > /dev/null
 
 # Make combined results
@@ -307,6 +326,7 @@ if [ ! ${OUTDIRRUN2} == "" ]; then
         jsamp=${isamp%_17.root}
         hadd -f ${jsamp}_run2.root ${jsamp}_16.root $isamp ${jsamp}_18.root > /dev/null
     done
+    rm *_1?.root
     popd > /dev/null
 fi
 
